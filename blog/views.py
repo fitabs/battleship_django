1a82c3059f6532819f9520d741206fc281422722
Gorox <gitmail@inbox.ru>
2017-02-21 01:10:18 +0300

    urls update for lesson MVA

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, render_to_response
+from django.http import HttpRequest, HttpResponse
+from blog.models import *
 # Create your views here.
+
+def home(request):
+    return render_to_response("blog/home.html")
+
+
+def artists(request):
+    artists = Artist.objects.all()
+    return render_to_response("blog/artists.html", {"artists": artists})
+
+
+def artistdetails(request, name):
+    a=2
