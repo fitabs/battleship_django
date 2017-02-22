51db2c0b304e1e04e4642bedbcb5aaa5cb4e466b
Gorox <gitmail@inbox.ru>
2017-02-23 01:32:12 +0300

    urls update for lesson MVA (template for all page)

--- blog/models.py
+++ blog/models.py
@@ -4,15 +4,16 @@ from django.forms import ModelForm
 class Artist(models.Model):
     name = models.CharField("artist", max_length=50)
     year_formed = models.PositiveIntegerField()
+    image = models.ImageField(upload_to='artist_img')
 
     def __str__(self):
         return str(self.id) +" "+ self.name
 
-class Album(models.Model):
-    name = models.CharField("album", max_length=50)
-    artist = models.ForeignKey(Artist)
-
 class ArtistForm(ModelForm):
     class Meta:
         model = Artist
-        fields = ['name', 'year_formed']
+        fields = ['name', 'year_formed', 'image']
+
+class Album(models.Model):
+    name = models.CharField("album", max_length=50)
+    artist = models.ForeignKey(Artist)
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, render_to_response
 from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
 from blog.models import *
+
 # Create your views here.
 
 def home(request):
-    return render_to_response("blog/coverTemplate.html")
+    return render_to_response("blog/home.html")
 
 
 def artists(request):
